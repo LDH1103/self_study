4d2bbf9873a00ba4957803e43a048e2a0006e324
LDH1103 <ldh517525@gmail.com>
2023-05-02 08:13:07 +0900

    23-05-02

--- PHP/board_project/src/common/db_common.php
+++ PHP/board_project/src/common/db_common.php
@@ -1,12 +1,5 @@
 <?php
 class Db_conn {
-    private $host = null;
-    private $user = null;
-    private $pass = null;
-    private $charset = null;
-    private $db_name = null;
-    private $dns = null;
-    private $pdo_option = null;
     private $param_conn = null;
 
     // ---------------------------------
@@ -16,13 +9,13 @@ class Db_conn {
     // 리턴값	: $this->param_conn
     // ---------------------------------
     public function __construct( &$param_conn ) {
-        $this->host = "localhost";
-        $this->user = "root";
-        $this->pass = "root506";
-        $this->charset = "utf8mb4";
-        $this->db_name = "board_project";
-        $this->dns = "mysql:host=".$this->host.";dbname=".$this->db_name.";charset=".$this->charset;
-        $this->pdo_option =
+        $host = "localhost";
+        $user = "root";
+        $pass = "root506";
+        $charset = "utf8mb4";
+        $db_name = "board_project";
+        $dns = "mysql:host=".$host.";dbname=".$db_name.";charset=".$charset;
+        $pdo_option =
             array(
                 PDO::ATTR_EMULATE_PREPARES         => false 
                 ,PDO::ATTR_ERRMODE                 => PDO::ERRMODE_EXCEPTION 
@@ -32,7 +25,7 @@ class Db_conn {
         try
         {
             echo "db연결 성공\n";
-            $this->param_conn = new PDO( $this->dns, $this->user, $this->pass, $this->pdo_option );
+            $this->param_conn = new PDO( $dns, $user, $pass, $pdo_option );
             return $this->param_conn;
         } catch( Exception $e) {   
             echo "에러 발생\n$e";
@@ -47,12 +40,7 @@ class Db_conn {
 }
 
 class Db_select {
-    private $sql = null;
-    private $arr_prepare = null;
     private $conn = null;
-    private $stmt = null;
-    private $result = null;
-    private $obj_db_conn = null;
 
     // ---------------------------------------------------
     // 함수명	: __construct
@@ -61,8 +49,8 @@ class Db_select {
     // 리턴값	: 없음
     // ---------------------------------------------------
     public function __construct() {
-        $this->obj_db_conn = new Db_conn( $this->obj_db_conn );
-        $this->conn = $this->obj_db_conn->get_param_conn();
+        $obj_db_conn = new Db_conn( $obj_db_conn );
+        $this->conn = $obj_db_conn->get_param_conn();
     }
     // ---------------------------------------------------
     // 함수명	: fnc_sel_board_all
@@ -71,7 +59,7 @@ class Db_select {
     // 리턴값	: Array/String      $this->result/ErrMSG
     // ---------------------------------------------------
     public function fnc_sel_board_all() {
-        $this->sql =
+        $sql =
         " SELECT "
         ."      * "
         ." FROM " 
@@ -80,18 +68,18 @@ class Db_select {
         ."      board_no DESC "
         ;
 
-        $this->arr_prepare = array();
+        $arr_prepare = array();
         try {
-            $this->stmt = $this->conn->prepare( $this->sql );
-            $this->stmt->execute( $this->arr_prepare );
-            $this->result = $this->stmt->fetchAll();
+            $stmt = $this->conn->prepare( $sql );
+            $stmt->execute( $arr_prepare );
+            $result = $stmt->fetchAll();
         } catch ( Exception $e ) {
             echo "에러 발생\n$e";
             throw new Exception( $e->getMessage() );
         } finally {
-            $this->conn = null;
+            $conn = null;
         }
-        return $this->result ;
+        return $result ;
     }
 }
 
